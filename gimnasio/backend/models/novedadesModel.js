47731c99378a93f4b5ad165e612046a0e2434191
FelipeAllendesM <allendesmunozf@gmail.com>
2025-05-03 02:16:02 -0300

    se agrega Crud Parte 2

--- gimnasio/backend/models/novedadesModel.js
+++ gimnasio/backend/models/novedadesModel.js
@@ -20,4 +20,32 @@ async function insertNovedad(obj) {
         throw error;
     }
 }
-module.exports = { getNovedades, insertNovedad }
+
+async function deleteNovedadById(id) {
+    var query = 'delete from novedades where id = ?';
+    var rows = await pool.query(query, [id]);
+    return rows;
+}
+
+async function getNovedadesById(id) {
+    try {
+        var query = 'select * from novedades where id = ?';
+        var rows = await pool.query(query, [id]);
+        return rows[0];
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+async function modificarNovedadesById(obj, id) {
+    try {
+        var query = 'update novedades set ? where id = ?';
+        var rows = await pool.query(query, [obj, id]);
+        return rows;
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+}
+
+module.exports = { getNovedades, insertNovedad, deleteNovedadById, getNovedadesById, modificarNovedadesById }
--- gimnasio/backend/routes/admin/novedades.js
+++ gimnasio/backend/routes/admin/novedades.js
@@ -41,5 +41,43 @@ router.post('/agregar', async (req, res, next) => {
     }
 });
 
+/* Eliminar Novedades*/
+router.get('/eliminar/:id', async (req, res, next) => {
+    var id = req.params.id;
+    await novedadesModel.deleteNovedadById(id);
+    res.redirect('/admin/novedades');
+});
+
+/*Formulario de novedades para modificar Datos*/
+router.get('/modificar/:id', async (req, res, next) => {
+    var id = req.params.id;
+    var novedad = await novedadesModel.getNovedadesById(id);
+
+    res.render('admin/modificar', {
+        layout: 'admin/layout',
+        novedad
+    });
+});
+
+/*Modificación de Novedades*/
+router.post('/modificar', async (req, res, next) => {
+    try {
+        var obj = {
+            titulo: req.body.titulo,
+            subtitulo: req.body.subtitulo,
+            cuerpo: req.body.cuerpo
+        }
+        await novedadesModel.modificarNovedadesById(obj, req.body.id);
+        res.redirect('/admin/novedades');
+    } catch (error) {
+        console.error(error)
+        res.render('admin/modificar', {
+            layout: 'admin/layout',
+            error: true,
+            message: 'No se modifico la Novedad'
+        })
+    }
+});
+
 
 module.exports = router;
