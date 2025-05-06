958114616ff178f0745a1f84a1a6438d09c41f89
FelipeAllendesM <allendesmunozf@gmail.com>
2025-05-05 22:42:41 -0300

    adaptacion react con api novedades

--- gimnasio/frontend/src/pages/NovedadesPage.js
+++ gimnasio/frontend/src/pages/NovedadesPage.js
@@ -1,13 +1,31 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import NovedadItem from '../components/novedades/NovedadItem'
+
 const NovedadesPage = (props) => {
+    const [loading, setLoading] = useState(false);
+    const [novedades, setNovedades] = useState([]);
+
+    useEffect(() => {
+        const cargarNovedades = async () => {
+            setLoading(true);
+            const response = await axios.get('http://localhost:3000/api/novedades');
+            setNovedades(response.data);
+            setLoading(false);
+        };
+        cargarNovedades();
+
+    }, []);
     return (
         <section className="holder">
-            <h2>Novedades</h2>
-            <div className="novedades">
-                <h3>Titulo</h3>
-                <h4>Subtitulo</h4>
-                <p>Cuerpo</p>
-            </div>
+            {loading ? (
+                <p>Cargando...</p>
+            ) : (
+                novedades.map(item => <NovedadItem key={item.id}
+                    title={item.titulo} subtitle={item.subtitulo}
+                    imagen={item.imagen} body={item.cuerpo} />)
+            )}
         </section>
     );
 }
