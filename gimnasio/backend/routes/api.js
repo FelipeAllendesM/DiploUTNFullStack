75d1347cf896e2c637baac171c2e19478ae5377c
FelipeAllendesM <allendesmunozf@gmail.com>
2025-05-09 00:31:45 -0300

    se adapta envio de correo

--- gimnasio/backend/routes/api.js
+++ gimnasio/backend/routes/api.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 var novedadesModel = require('./../models/novedadesModel');
 var cloudinary = require('cloudinary').v2;
+var nodemailer = require('nodemailer');
+
 
 router.get('/novedades', async function (req, res, next) {
     var novedades = await novedadesModel.getNovedades();
@@ -26,5 +28,29 @@ router.get('/novedades', async function (req, res, next) {
     });
     res.json(novedades);
 });
+
+router.post('/contacto', async (req, res) => {
+    const mail = {
+        to: 'allendesmunozf@gmail.com',
+        subject: 'Contacto Web',
+        html: `${req.body.nombre} se contacto a traves del sitio web y quiere mayor información a este correo: ${req.body.email} <br> Ademas realizo el siguiente comentario: ${req.body.mensaje} <br> Sutelefono es el siguiente: ${req.body.telefono}`
+    }
+
+    const transport = nodemailer.createTransport({
+        host: process.env.SMTP_HOST,
+        port: process.env.SMTP_PORT,
+        auth: {
+            user: process.env.SMTP_USER,
+            pass: process.env.SMTP_PASS
+        }
+    })
+    await transport.sendMail(mail)
+
+    res.status(201).json({
+        error: false,
+        message: 'Mensaje Enviado'
+    });
+});
+
 
 module.exports = router;
